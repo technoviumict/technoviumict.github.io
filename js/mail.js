16416e638e54cf339c74e95e7d9b6bd9138cb549
davorradic <44604843+davorradic@users.noreply.github.com>
2020-03-03 11:13:42 +0100

    first commit

---
+++ js/mail.js
@@ -0,0 +1,28 @@
+//$( document ).ready(function() {
+
+    $('#mc-form').ajaxChimp({
+        url: 'https://roc-nijmegen.us4.list-manage.com/subscribe/post?u=e8f65e4efc65dd48bc3b38ced&amp;id=e7a79f6070'
+    });
+/*
+    $( "#whitehomepage" ).click(function() {
+    event.preventDefault();
+
+    $.ajax({
+        url: "https://roc-nijmegen.us4.list-manage.com/subscribe/post?u=e8f65e4efc65dd48bc3b38ced&amp;id=e7a79f6070",
+        type: "POST",
+        crossDomain: true,
+        contentType: 'application/json',
+        data: $form.serialize(),
+        dataType: "jsonp",
+        success: function(data) {
+            console.log('success')
+            //success handler
+        },
+        error: function() {
+            //error handler
+            console.log('error')
+        }
+    });
+    });
+    */
+//});
